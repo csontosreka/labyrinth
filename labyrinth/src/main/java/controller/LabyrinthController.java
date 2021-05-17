f18b9a707c1478058a2e3a92bfa4908b6921deb3
csontosreka <61116319+csontosreka@users.noreply.github.com>
2021-05-17 17:56:50 +0200

    Added LabyrinthController

---
+++ labyrinth/src/main/java/controller/LabyrinthController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import javafx.beans.binding.ObjectBinding;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import model.LabyrinthModel;
+
+public class LabyrinthController {
+    @FXML
+    private GridPane board;
+
+    private LabyrinthModel model = new LabyrinthModel();
+
+    @FXML
+    private void initialize() {
+        for (int i = 0; i < board.getRowCount(); i++) {
+            for (int j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare(i, j);
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare(int i, int j) {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        var piece = new Circle(50);
+
+        piece.fillProperty().bind(
+                new ObjectBinding<Paint>() {
+                    {
+                        super.bind(model.squareProperty(i, j));
+                    }
+                    @Override
+                    protected Paint computeValue() {
+                        return switch (model.squareProperty(i, j).get()) {
+                            case POS -> Color.BLACK;
+                            default -> Color.TRANSPARENT;
+                        };
+                    }
+                }
+        );
+        square.getChildren().add(piece);
+        square.setOnMouseClicked(this::handleMouseClick);
+        return square;
+    }
+
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        var square = (StackPane) event.getSource();
+        var row = GridPane.getRowIndex(square);
+        var col = GridPane.getColumnIndex(square);
+        System.out.printf("Click on square (%d,%d)\n", row, col);
+        model.move(row, col);
+    }
+}
